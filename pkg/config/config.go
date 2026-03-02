cc0a8d4ccdcd8529110caf6706d95bd754f7ec41
Nixxik <v.gorbachev05@yandex.ru>
2026-03-02 14:12:21 +0700

    Написание конфига и логгера, доработка структуры проекта

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	TGToken string `mapstructure:"TG_TOKEN`
+	AppEnv  string `mapstructure:"APP_ENV"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	v := viper.New()
+	v.SetConfigFile(path)
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("config.LoadConfig: ошибка чтения конфига %w", err)
+	}
+	var cfg Config
+	if err := v.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("config.LoadConfig: Ошибка анмаршала конфига %w", err)
+	}
+	return &cfg, nil
+}
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,36 @@
 package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	EnvLocal = "local"
+	EnvDev   = "dev"
+)
+
+func Setup(env string) {
+	var handler slog.Handler
+
+	switch env {
+	case EnvLocal:
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		})
+	case EnvDev:
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		})
+	default:
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		})
+	}
+
+	slog.SetDefault(slog.New(handler))
+}
+
+func With(args ...any) *slog.Logger {
+	return slog.With(args...)
+}
